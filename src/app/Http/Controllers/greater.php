d03d3c7e815ba291c2ac5a7ce879c810197c02f7
Jeff <jeffery.ahern@gmail.com>
2023-02-26 22:51:02 -0700

    init commit

---
+++ src/app/Http/Controllers/greater.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class greater extends Controller
+{
+    /**
+     * Handle the incoming request. and return the string with a hello and then the request name value
+     */
+    public function __invoke(Request $request)
+    {
+        //git init
+    }
+}
